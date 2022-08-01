fcd81c8107b01ae72113f307721695db6f201dfe
galaxyAbstractor <galaxyAbstractor@gmail.com>
2022-08-01 18:27:55 +0200

    Improved animations

--- nollebricka.ino
+++ nollebricka.ino
@@ -1,7 +1,8 @@
 // Based on code from https://github.com/GeorgiZhelezov/arduino-msgeq7
 
 #include <FastLED.h>
-#define NUM_LEDS 69
+
+#define NUM_LEDS 70
 #define LED_PIN 2
 CRGB leds[NUM_LEDS];
 
@@ -12,64 +13,125 @@ unsigned short resetPin = 9;
 unsigned short MSGEQPin = 1;
 unsigned short noiseFilter = 300;
 unsigned short readDataDelay = 3; //delay after reading MSGEQ7 data
+uint8_t hue = 0;
 
-int ledSections[7][4] = {
-  {0,9, 0, 255},
-  {10, 19, 64, 255},
-  {20, 29, 96, 255},
-  {30, 39, 128, 255},
-  {40, 49, 160, 255},
-  {50, 59, 192, 255},
-  {60, 69, 224, 255}
+int ledSections[7][2][2] = {
+        {
+                {0,  4},
+                {65, 69}
+        },
+        {
+                {5,  9},
+                {60, 64}
+        },
+        {
+                {10, 14},
+                {55, 59}
+        },
+        {
+                {15, 19},
+                {50, 54}
+        },
+        {
+                {20, 24},
+                {45, 49}
+        },
+        {
+                {25, 29},
+                {40, 44}
+        },
+        {
+                {30, 34},
+                {35, 39}
+        }
 };
 
 void setup() {
-  Serial.begin(9600);
+    // put your setup code here, to run once:
+    FastLED.addLeds<WS2812, LED_PIN>(leds, NUM_LEDS);
   
-  // put your setup code here, to run once:
-  FastLED.addLeds<WS2812, LED_PIN>(leds, NUM_LEDS);
+    pinMode(strobePin, OUTPUT);
+    pinMode(resetPin, OUTPUT);
+    pinMode(MSGEQPin, INPUT);
 
-  pinMode(strobePin, OUTPUT);
-  pinMode(resetPin, OUTPUT);
-  pinMode(MSGEQPin, INPUT);
-
-  digitalWrite(resetPin, HIGH);
-  digitalWrite(resetPin, LOW);
+    digitalWrite(resetPin, HIGH);
+    digitalWrite(resetPin, LOW);
 
 }
 
 void loop() {
 
-  readData();
-  
+    readData();
+    rainbowEq();
 
 }
 
-void readData() 
-{
-  for (short i = 0; i < 7; i++)
-  {
-    digitalWrite(strobePin, LOW); 
+void readData() {
+    for (short i = 0; i < 7; i++) {
+        digitalWrite(strobePin, LOW);
 
-    MSGEQ[i] = analogRead(MSGEQPin); 
+        MSGEQ[i] = analogRead(MSGEQPin);
 
-    digitalWrite(strobePin, HIGH); 
+        digitalWrite(strobePin, HIGH);
 
-    MSGEQ[i] = constrain(MSGEQ[i], noiseFilter, 1023); 
-   
-    MSGEQ[i] = map(MSGEQ[i], noiseFilter, 1023, 0, 255); 
+        MSGEQ[i] = constrain(MSGEQ[i], noiseFilter, 1023);
 
-    delay(readDataDelay);
+        MSGEQ[i] = map(MSGEQ[i], noiseFilter, 1023, 0, 255);
 
-    led(i, MSGEQ[i]);
-     
-  }
+        delay(readDataDelay);
 
-}
 
-void led(short sectionIndex, unsigned short brightness) {
-    for(int dot = ledSections[sectionIndex][0]; dot <= ledSections[sectionIndex][1]; dot++) { 
-            leds[dot] = CHSV(ledSections[sectionIndex][2], ledSections[sectionIndex][3], brightness);
     }
+
+}
+
+void rainbowEq() {
+    for (int sectionIndex = 0; sectionIndex < 7; sectionIndex++) {
+        unsigned short brightness = MSGEQ[sectionIndex];
+        brightness = constrain(brightness, 10, 255);
+        
+        for (int side = 0; side < 2; side++) {
+            for (int dot = ledSections[sectionIndex][side][0]; dot <= ledSections[sectionIndex][side][1]; dot++) {
+              int thisSection = 32 * sectionIndex;
+              int nextSection = 32 * (sectionIndex + 1);
+
+              // To make each section fade to next section
+              int extra = map(dot, ledSections[sectionIndex][side][0], ledSections[sectionIndex][side][1], 0, nextSection - thisSection - 1);
+
+              // Invert on other side
+              if (side == 1) {
+                extra = (extra * -1) + 32;
+              }
+              
+              leds[dot] = CHSV(thisSection + extra, 255, brightness);
+            }
+        }
+    }
+    
     FastLED.show();
+}
+
+void whiteEq() {
+    for (int sectionIndex = 0; sectionIndex < 7; sectionIndex++) {
+        unsigned short brightness = MSGEQ[sectionIndex];
+        brightness = constrain(brightness, 10, 100);
+        
+        for (int side = 0; side < 2; side++) {
+            for (int dot = ledSections[sectionIndex][side][0]; dot <= ledSections[sectionIndex][side][1]; dot++) {
+              leds[dot] = CHSV(0, 0, brightness);
+            }
+        }
+    }
+    
+    FastLED.show();
+}
+
+void rainbowFade() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(hue++ + i, 255, 50);
+    FastLED.show();
+    delay(30);
+  }
+  
+  
 }
