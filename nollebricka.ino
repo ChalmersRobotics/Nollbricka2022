e90dc2c1d97ac01d58157e659adda9fbafec6618
Victor Nagy <galaxyAbstractor@gmail.com>
2022-08-03 18:57:57 +0200

    Added buttons

--- nollebricka.ino
+++ nollebricka.ino
@@ -1,58 +1,66 @@
 // Based on code from https://github.com/GeorgiZhelezov/arduino-msgeq7
 
 #include <FastLED.h>
+#include <Button.h>
 
-#define NUM_LEDS 70
+#define NUM_LEDS 67
 #define LED_PIN 2
 CRGB leds[NUM_LEDS];
 
+Button modeButton(6); 
 
 unsigned short MSGEQ[7]; //arrays for storing MSGEQ7 data
 unsigned short strobePin = 4;
 unsigned short resetPin = 9;
+unsigned short noiseFilterPin = A2;
 unsigned short MSGEQPin = 1;
-unsigned short noiseFilter = 300;
 unsigned short readDataDelay = 3; //delay after reading MSGEQ7 data
 uint8_t hue = 0;
 
+uint8_t mode = 0;
+boolean pressed = false;
+
+
 int ledSections[7][2][2] = {
         {
-                {0,  4},
-                {65, 69}
+                {0,  3},
+                {63, 66}
         },
         {
-                {5,  9},
-                {60, 64}
+                {4,  7},
+                {59, 62}
         },
         {
-                {10, 14},
-                {55, 59}
+                {8, 11},
+                {55, 58}
         },
         {
-                {15, 19},
-                {50, 54}
+                {12, 15},
+                {51, 54}
         },
         {
-                {20, 24},
-                {45, 49}
+                {16, 19},
+                {47, 50}
         },
         {
-                {25, 29},
-                {40, 44}
+                {20, 23},
+                {43, 46}
         },
         {
-                {30, 34},
-                {35, 39}
+                {24, 33},
+                {34, 42}
         }
 };
 
 void setup() {
+    modeButton.begin();
     // put your setup code here, to run once:
     FastLED.addLeds<WS2812, LED_PIN>(leds, NUM_LEDS);
   
     pinMode(strobePin, OUTPUT);
     pinMode(resetPin, OUTPUT);
     pinMode(MSGEQPin, INPUT);
+    pinMode(noiseFilterPin, INPUT);
 
     digitalWrite(resetPin, HIGH);
     digitalWrite(resetPin, LOW);
@@ -61,12 +69,41 @@ void setup() {
 
 void loop() {
 
-    readData();
-    rainbowEq();
+    if (modeButton.pressed() && !pressed) {
+        pressed = true;
+        mode++;
+
+        if (mode >= 4) {
+          mode = 0;
+        }
+    }
+
+    if (modeButton.released()) {
+      pressed = false;
+    }
+
+    if (mode == 0) {
+      readData();
+      rainbowEq();
+    } else if (mode == 1) {
+      readData();
+      rainbowEqMinBrightness();
+    } else if (mode == 2) {
+      readData();
+      whiteEq();
+    } else if (mode == 3) {
+      rainbowFade();
+    }   
 
 }
 
 void readData() {
+    unsigned short noiseFilter = analogRead(noiseFilterPin);
+    
+    delay(readDataDelay);
+
+    noiseFilter = constrain(noiseFilter, 50, 1023);
+
     for (short i = 0; i < 7; i++) {
         digitalWrite(strobePin, LOW);
 
@@ -79,13 +116,36 @@ void readData() {
         MSGEQ[i] = map(MSGEQ[i], noiseFilter, 1023, 0, 255);
 
         delay(readDataDelay);
-
-
     }
 
 }
 
 void rainbowEq() {
+    for (int sectionIndex = 0; sectionIndex < 7; sectionIndex++) {
+        unsigned short brightness = MSGEQ[sectionIndex];
+        
+        for (int side = 0; side < 2; side++) {
+            for (int dot = ledSections[sectionIndex][side][0]; dot <= ledSections[sectionIndex][side][1]; dot++) {
+              int thisSection = 32 * sectionIndex;
+              int nextSection = 32 * (sectionIndex + 1);
+
+              // To make each section fade to next section
+              int extra = map(dot, ledSections[sectionIndex][side][0], ledSections[sectionIndex][side][1], 0, nextSection - thisSection - 1);
+
+              // Invert on other side
+              if (side == 1) {
+                extra = (extra * -1) + 32;
+              }
+              
+              leds[dot] = CHSV(thisSection + extra, 255, brightness);
+            }
+        }
+    }
+    
+    FastLED.show();
+}
+
+void rainbowEqMinBrightness() {
     for (int sectionIndex = 0; sectionIndex < 7; sectionIndex++) {
         unsigned short brightness = MSGEQ[sectionIndex];
         brightness = constrain(brightness, 10, 255);
@@ -128,7 +188,7 @@ void whiteEq() {
 
 void rainbowFade() {
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CHSV(hue++ + i, 255, 50);
+    leds[i] = CHSV(hue++ + i, 255, 100);
     FastLED.show();
     delay(30);
   }
